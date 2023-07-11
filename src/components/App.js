ad6836b974926828bbc07788e54663b8c50117ad
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-07-11 08:46:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[clickedBtn, setClickedBtn]=useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+    <p>Button clicked {clickedBtn} times</p>
+    <button onClick={()=> setClickedBtn(clickedBtn+1)}>Click me</button>
     </div>
   )
 }
